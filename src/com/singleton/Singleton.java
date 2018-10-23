7c05d95de95b09f38daf52bb523ff5ada2f0f859
Sandeep Kumar <sandeepkumar@live.in>
2018-10-23 12:26:43 +0530

    initial commit

--- src/com/singleton/Singleton.java
+++ src/com/singleton/Singleton.java
@@ -1,5 +1,43 @@
 package com.singleton;
 
+import java.lang.reflect.ReflectPermission;
+import java.security.Permission;
+
 public class Singleton {
+
+	private static Singleton singletonObject = null;
+
+	private Singleton() {
+
+		System.setSecurityManager(new SecurityManager() {
+			@Override
+			public void checkPermission(Permission perm) {
+				if (perm.getClass() == ReflectPermission.class && "suppressAccessChecks".equals(perm.getName())) {
+					throw new SecurityException("can't supress AccessChecks");
+				}
+			}
+		});
+		/*if (singletonObject != null) {
+			throw new RuntimeException("Not Allowed !!");
+		}*/
+
+	}
+
+	public static Singleton getInstance() {
+
+		if (singletonObject == null) {
+			synchronized (Singleton.class) {
+
+				if (singletonObject == null) {
+
+					singletonObject = new Singleton();
+
+				}
+			}
+		}
+
+		return singletonObject;
+
+	}
 
 }
--- src/com/singleton/Test.java
+++ src/com/singleton/Test.java
@@ -1,9 +1,25 @@
 package com.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, InstantiationException,
+			IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		// TODO Auto-generated method stub
+
+		// By Reflection
+		Constructor c = Singleton.class.getDeclaredConstructor();
+		c.setAccessible(true);
+		Singleton singObj = (Singleton) c.newInstance();
+		System.out.println("Reflection Obj" + singObj.hashCode());
+
+		Singleton obj = Singleton.getInstance();
+		Singleton obj1 = Singleton.getInstance();
+
+		System.out.println("OBJ 1" + obj.hashCode());
+		System.out.println("OBJ 2" + obj1.hashCode());
 
 	}
 
